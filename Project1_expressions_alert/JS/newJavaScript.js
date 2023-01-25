3f498ffda99a1cd31fc1e2b118ce09f6663261bc
IsaacJSaccary <118865604+IsaacJSaccary@users.noreply.github.com>
2023-01-25 15:22:43 -0500

    Operator and Functions

--- Project1_expressions_alert/JS/newJavaScript.js
+++ Project1_expressions_alert/JS/newJavaScript.js
@@ -1,1 +1,5 @@
-document.write(600 * 430);   //Multiplying two numbers
+window.alert("WARNING"); //This will display a Window alert that says WARNING.
+var str = Sent1 = "This is the beginning of the string"; //This code assigns the string Sent1 with the sentence between the quotations.
+var str = Sent2 = " and this is the end of the string"; //This code assigns the string Sent2 with the sentence between the quotations.
+document.write(Sent1 + Sent2); //This code displays Sent1 and Sent2 together.
+6 + 7; //This adds 6 + 7, but does not display it because it's not a statement.
